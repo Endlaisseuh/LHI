c631da1beca64f81fe69ff5cd0b20c23be0a7a40
Endlaisseuh <jeux.thomas07@gmail.com>
2026-07-23 09:32:52 +0200

    Update ToolManager.js

--- js/managers/ToolManager.js
+++ js/managers/ToolManager.js
@@ -12,19 +12,51 @@ LHI.ToolManager = class {
     };
 
     constructor() {
+
         this.currentTool = LHI.ToolManager.TOOLS.PAN;
+
+        this.listeners = [];
+
     }
 
     setTool(tool) {
+
+        if (this.currentTool === tool) {
+            return;
+        }
+
         this.currentTool = tool;
+
+        this.emit();
+
     }
 
     getTool() {
+
         return this.currentTool;
+
     }
 
     is(tool) {
+
         return this.currentTool === tool;
+
+    }
+
+    onChange(callback) {
+
+        this.listeners.push(callback);
+
+    }
+
+    emit() {
+
+        this.listeners.forEach(callback => {
+
+            callback(this.currentTool);
+
+        });
+
     }
 
 };
